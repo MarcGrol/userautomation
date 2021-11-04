87f03d00a71a74df773340e617f969d97ed0b2d7
marcgrol <mgrol@xebia.com>
2021-11-04 09:21:41 +0100

    fix lock within lock

--- realtime/realtimeactions/logging.go
+++ realtime/realtimeactions/logging.go
@@ -8,7 +8,7 @@ import (
 )
 
 var logFunc = func(ctx context.Context, ruleName string, userStatus realtimecore.UserStatus, user realtimecore.User) error {
-	fmt.Printf("Action for rule '%s' triggered action om User '%s' - status: %+v\n", ruleName, user.FullName, userStatus)
+	fmt.Printf("Action for rule '%s' triggered action om User '%s' - status: %+v\n", ruleName, user.UID, userStatus)
 	return nil
 }
 
