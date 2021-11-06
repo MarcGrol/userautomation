38372fc51f437abe57d427a5761420598637fae3
marcgrol <mgrol@xebia.com>
2021-11-07 00:56:26 +0100

    Made segment-service transactional

--- services/segmentservice/user_segment_service.go
+++ services/segmentservice/user_segment_service.go
@@ -44,146 +44,184 @@ func (s *segmentService) OnEvent(ctx context.Context, topic string, event interf
 }
 
 func (s *segmentService) OnUserCreated(ctx context.Context, u user.User) error {
-	// check if user must be added to segments
-	segments, err := s.segmentStore.GetAll(ctx)
-	if err != nil {
-		return err
-	}
-
-	for _, item := range segments {
-		segm := item.(segment.UserSegment)
-		isApplicable, err := segm.IsApplicableForUser(ctx, u)
+	return s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		// check if user must be added to segments
+		segments, err := s.segmentStore.GetAll(ctx)
 		if err != nil {
 			return err
 		}
-		if isApplicable {
-			segm.Users[u.UID] = u
-			err := s.segmentStore.Put(ctx, segm.UID, segm)
+
+		for _, item := range segments {
+			segm := item.(segment.UserSegment)
+			isApplicable, err := segm.IsApplicableForUser(ctx, u)
 			if err != nil {
 				return err
 			}
+			if isApplicable {
+				segm.Users[u.UID] = u
+				err := s.segmentStore.Put(ctx, segm.UID, segm)
+				if err != nil {
+					return err
+				}
+			}
 		}
-	}
-	return nil
+		return nil
+	})
 }
 
 func (s *segmentService) OnUserModified(ctx context.Context, _ user.User, u user.User) error {
-	// check if user must be added to segments
-	segments, err := s.segmentStore.GetAll(ctx)
-	if err != nil {
-		return err
-	}
+	return s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
 
-	for _, item := range segments {
-		segm := item.(segment.UserSegment)
-		_, found := segm.Users[u.UID]
-		isApplicable, err := segm.IsApplicableForUser(ctx, u)
+		segments, err := s.segmentStore.GetAll(ctx)
 		if err != nil {
 			return err
 		}
-		if found && !isApplicable {
-			err = s.segmentStore.Remove(ctx, segm.UID)
+
+		for _, item := range segments {
+			segm := item.(segment.UserSegment)
+			_, found := segm.Users[u.UID]
+			isApplicable, err := segm.IsApplicableForUser(ctx, u)
 			if err != nil {
 				return err
 			}
-		} else if isApplicable {
-			segm.Users[u.UID] = u
+			if found && !isApplicable {
+				err = s.segmentStore.Remove(ctx, segm.UID)
+				if err != nil {
+					return err
+				}
+			} else if isApplicable {
+				segm.Users[u.UID] = u
+				err = s.segmentStore.Put(ctx, segm.UID, segm)
+				if err != nil {
+					return err
+				}
+
+			}
+		}
+		return nil
+	})
+}
+
+func (s *segmentService) OnUserRemoved(ctx context.Context, u user.User) error {
+	return s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		// check if user must be added to segments
+		segments, err := s.segmentStore.GetAll(ctx)
+		if err != nil {
+			return err
+		}
+
+		for _, item := range segments {
+			segm := item.(segment.UserSegment)
+			delete(segm.Users, u.UID)
 			err = s.segmentStore.Put(ctx, segm.UID, segm)
 			if err != nil {
 				return err
 			}
-
 		}
-	}
-	return nil
-}
 
-func (s *segmentService) OnUserRemoved(ctx context.Context, u user.User) error {
-	// check if user must be added to segments
-	segments, err := s.segmentStore.GetAll(ctx)
-	if err != nil {
-		return err
-	}
-
-	for _, item := range segments {
-		segm := item.(segment.UserSegment)
-		delete(segm.Users, u.UID)
-		err = s.segmentStore.Put(ctx, segm.UID, segm)
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+		return nil
+	})
 }
 
 func (s *segmentService) Put(ctx context.Context, segm segment.UserSegment) error {
-	users, err := s.userService.Query(ctx, segm.IsApplicableForUser)
-	if err != nil {
-		return err
-	}
-
-	// recalculate list of users
-	segm.Users = map[string]user.User{}
-	for _, u := range users {
-		applicable, err := segm.IsApplicableForUser(ctx, u)
+	return s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		users, err := s.userService.Query(ctx, segm.IsApplicableForUser)
 		if err != nil {
 			return err
 		}
-		if applicable {
-			segm.Users[u.UID] = u
-		}
-	}
 
-	if len(segm.Users) > 0 {
-		err = s.segmentStore.Put(ctx, segm.UID, segm)
-		if err != nil {
-			return err
+		// recalculate list of users
+		segm.Users = map[string]user.User{}
+		for _, u := range users {
+			applicable, err := segm.IsApplicableForUser(ctx, u)
+			if err != nil {
+				return err
+			}
+			if applicable {
+				segm.Users[u.UID] = u
+			}
 		}
-	}
 
-	return nil
+		if len(segm.Users) > 0 {
+			err = s.segmentStore.Put(ctx, segm.UID, segm)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 }
 
 func (s *segmentService) Get(ctx context.Context, userSegmentUID string) (segment.UserSegment, bool, error) {
-	item, exists, err := s.segmentStore.Get(ctx, userSegmentUID)
-	return item.(segment.UserSegment), exists, err
+	var segm segment.UserSegment
+	segmentExists := false
+	err := s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		item, exists, err := s.segmentStore.Get(ctx, userSegmentUID)
+		if err != nil {
+			return err
+		}
+		segm = item.(segment.UserSegment)
+		segmentExists = exists
+
+		return nil
+	})
+	if err != nil {
+		return segm, false, err
+	}
+	return segm, segmentExists, nil
 }
 
 func (s *segmentService) List(ctx context.Context) ([]segment.UserSegment, error) {
-	items, err := s.segmentStore.GetAll(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	segments := []segment.UserSegment{}
-	for _, i := range items {
-		segments = append(segments, i.(segment.UserSegment))
-	}
 
+	err := s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		items, err := s.segmentStore.GetAll(ctx)
+		if err != nil {
+			return err
+		}
+
+		segments := []segment.UserSegment{}
+		for _, i := range items {
+			segments = append(segments, i.(segment.UserSegment))
+		}
+
+		return nil
+	})
+	if err != nil {
+		return segments, err
+	}
 	return segments, nil
 }
 
 func (s *segmentService) Remove(ctx context.Context, userSegmentUID string) error {
-	// Can we remove a segment? I might still be in use by a rule
+	return s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		// Can we remove a segment? I might still be in use by a rule
 
-	return fmt.Errorf("Not implemented")
+		return fmt.Errorf("Not implemented")
+	})
 }
 
 func (s *segmentService) GetUsersForSegment(ctx context.Context, userSegmentUID string) ([]user.User, error) {
-	item, exists, err := s.segmentStore.Get(ctx, userSegmentUID)
-	if err != nil {
-		return nil, err
-	}
-	if !exists {
-		return nil, fmt.Errorf("Segment with uid %s does not exist", userSegmentUID)
-	}
-
-	segm := item.(segment.UserSegment)
 	users := []user.User{}
-	for _, u := range segm.Users {
-		users = append(users, u)
-	}
+	err := s.segmentStore.RunInTransaction(ctx, func(ctx context.Context) error {
+		item, exists, err := s.segmentStore.Get(ctx, userSegmentUID)
+		if err != nil {
+			return err
+		}
+		if !exists {
+			return fmt.Errorf("Segment with uid %s does not exist", userSegmentUID)
+		}
 
+		segm := item.(segment.UserSegment)
+		users := []user.User{}
+		for _, u := range segm.Users {
+			users = append(users, u)
+		}
+		return nil
+	})
+	if err != nil {
+		return users, err
+	}
 	return users, nil
 }
