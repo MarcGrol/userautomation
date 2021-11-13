a908971cdf7502b56769aeaec686bf8115d7bb93
marcgrol <mgrol@xebia.com>
2021-11-14 00:08:33 +0100

    debug moore

--- core/user/user.go
+++ core/user/user.go
@@ -16,7 +16,7 @@ func (u User) HasAttributes(attributes []string) bool {
 	for _, attr := range attributes {
 		_, exists := u.Attributes[attr]
 		if !exists {
-			log.Printf("Missing mandatory attribute %s", attr)
+			log.Printf("Missing mandatory attribute %s for user %s", attr, u)
 			return false
 		}
 	}
